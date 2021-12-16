083c984577c59e25c8fd963116bf597c6caec491
Lauro Obed Nolasco Vazquez <isc18350511@gmail.com>
2021-12-15 22:58:29 -0600

    Definiendo Primeras Queries

---
+++ models/usuarios.js
@@ -0,0 +1,25 @@
+const usuariosQueries = {
+    insertUsuario: `
+      INSERT INTO
+        usuarios(
+          nombre,
+          email,
+          password,
+          status
+        )
+      VALUES
+        (?, ?, ?, ?)
+    `,
+    selectUsuarios: `
+      SELECT
+        *
+      FROM
+        usuarios
+      WHERE
+        status = 1
+      LIMIT
+        ?, ?
+    `,
+  };
+  
+  
